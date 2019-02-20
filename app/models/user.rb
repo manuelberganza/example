b1afd83d306fad6275b8c697049c3e2814b22269
Manuel Berganza <berganzamanuel@gmail.com>
2019-02-20 10:54:13 -0600

    Start

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  has_many :messages
+  validates :email, uniqueness: true
+  validates :nick, presence: true
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:email) { |n| "user#{n}@factory.com"}
+    nick Faker::TvShows::GameOfThrones.character
+  trait :with_messages do
+    after(:create) do |user|
+    3.times { create(:message, user: user)}
+      end
+    end
+    factory :user_with_messages, traits: [:with_messages]
+  end
+end
